830770ee453d0a8673a3929d9005b9ca25199de1
harunobu-tsutsumi <h_tsutsumi@innovation.co.jp>
2022-11-16 14:52:51 +0900

    インデックスを貼る

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -17,7 +17,9 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    INDEX index_popularity (popularity),
+    INDEX index_rend (rent)
 );
 
 CREATE TABLE isuumo.chair
@@ -34,5 +36,7 @@ CREATE TABLE isuumo.chair
     features    VARCHAR(64)     NOT NULL,
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
-    stock       INTEGER         NOT NULL
+    stock       INTEGER         NOT NULL,
+    INDEX index_popularity (popularity),
+    INDEX index_price (price)
 );
